585067a8781064e72acc69de358b5aefe5a9ba90
web weaver <127326749+servant-of-Allah@users.noreply.github.com>
2023-09-15 16:04:58 +0600

    Update Button.jsx

--- components/Button.jsx
+++ components/Button.jsx
@@ -8,7 +8,7 @@ const Button = () => {
     <div>
       <button
         type="button"
-        className="bg-transparent uppercase text-white font-semibold tracking-widest text-[17px] border-[1px] border-white rounded-md md:rounded-lg px-10 py-2 transition-[100ms] hover:border-dark block mx-auto md:mr-12"
+        className="bg-transparent uppercase text-white font-semibold tracking-widest text-[17px] border-[1px] border-white rounded-md md:rounded-lg px-10 py-2 transition-[100ms] hover:border-dark block mx-auto md:mr-12 mb-8 sm:mb-10"
         onClick={() => setShowRule((r) => !r)}
       >
         rules
